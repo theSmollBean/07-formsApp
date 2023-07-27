0d21de3f55eb7ea33512c519819be84dbff179c0
Alheli Cabrera <alecab1308@gmail.com>
2023-07-27 12:40:34 -0600

    routing the site and starting the functional menu.

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -3,19 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
-import { DinamicPageComponent } from './pages/dinamic-page/dinamic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     BasicPageComponent,
-    DinamicPageComponent,
+    DynamicPageComponent,
     SwitchesPageComponent
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class ReactiveModule { }
