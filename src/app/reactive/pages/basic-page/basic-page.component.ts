5bbd0a319558c6415d4f45096010cb3a5c009a50
Alheli Cabrera <alecab1308@gmail.com>
2023-07-27 13:07:54 -0600

    reset and oninit for starting the form with values

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
+const rtx5090 = {
+  name: 'RTX',
+    price: 2500,
+    inStorage: 6,
+}
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl('', [], []),
   //   price: new FormControl(0, [], []),
@@ -14,16 +19,24 @@ export class BasicPageComponent {
   // });
 
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0]
+    name: ['', [ Validators.required, Validators.minLength(3) ] ],
+    price: [0, [ Validators.required, Validators.min(0) ] ],
+    inStorage: [0, [ Validators.required, Validators.min(0) ] ],
   })
 
   constructor( private fb: FormBuilder ){}
 
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
+
   onSave(): void{
 
+    if ( this.myForm.invalid ) return;
+
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 10, inStorage: 0 });
 
   }
 }
